02970f9af37efdd46171541ad6e153a37098df21
Priya-shan <Shanmugapriyashanu2002@gmail.com>
2022-01-15 20:07:08 +0530

    commit

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app=express();
 let port = process.envnPORT || 3000;
 
 app.get("/",(req,res)=>{
+    res.send("hello");
+});
+app.get("/hospitals",(req,res)=>{
     res.send(importdata);
 });
 
 app.listen(port,()=>{
     console.log(`listening on port http://localhost:${port}`);
-})
+});
